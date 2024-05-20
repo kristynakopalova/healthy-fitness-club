0591bd1779ff16afac2e46120f2f7688925ad67d
Kristýna Kopalová <kopalova30@gmail.com>
2024-05-20 22:32:45 +0200

    editing style of the form

--- src/components/entry-form/EntryForm.jsx
+++ src/components/entry-form/EntryForm.jsx
@@ -25,131 +25,101 @@ export function EntryForm() {
     <div>
       <Header />
       <div className="entry-form">
-        <h2>Vstupní formulář</h2>
+        <h2>Vstupní formulář Healthy fitness clubu</h2>
         <form onSubmit={handleSubmit}>
           <div className="form-columns">
-              {/* Added div for two-column layout */}
-            <div className="column">
-              <label htmlFor="">
-                Titul:
-                <input
-                  type="text"
-                  name="titul"
-                  id="titul"
-                  value={inputs.titul || ''}
-                  onChange={handleChange}
-                />
-              </label>
-              <label htmlFor="">
-                Křestní jméno:
-                <input
-                  type="text"
-                  name="jmeno"
-                  id="jmeno"
-                  value={inputs.jmeno || ''}
-                  onChange={handleChange}
-                />
-              </label>
-              <label htmlFor="">
-                Příjmení:
-                <input
-                  type="text"
-                  name="prijmeni"
-                  id="prijmeni"
-                  value={inputs.prijmeni || ''}
-                  onChange={handleChange}
-                />
-              </label>
-              <label htmlFor="">
-                Email:
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={inputs.email || ''}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <div className="column">
-              <label htmlFor="">
-                Mobil (ve formátu 123456789):
-                <input
-                  type="tel"
-                  name="mobil"
-                  id="mobil"
-                  pattern="[0-9]{9}"
-                  value={inputs.mobil || ''}
-                  onChange={handleChange}
-                />
-              </label>
-              <label htmlFor="">
-                Datum narození:
-                <input
-                  type="date"
-                  name="datum"
-                  id="datum"
-                  value={inputs.datum || ''}
-                  onChange={handleChange}
-                />
-              </label>
-              <label htmlFor="">
-                Výška v cm:
-                <input
-                  type="number"
-                  name="vyska"
-                  id="vyska"
-                  value={inputs.vyska || ''}
-                  onChange={handleChange}
-                />
-              </label>
-              <label htmlFor="">
-                Hmotnost v kg:
-                <input
-                  type="number"
-                  name="hmotnost"
-                  id="hmotnost"
-                  value={inputs.hmotnost || ''}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-          </div>
-          <label htmlFor="">
-            Mám zájem o:
-            <input
-              type="checkbox"
-              name="konzultace"
-              id="konzultace"
-              onChange={handleChangeCheckbox}
-            />
-            <label for="konzultace">
-              Konzultace, testování, preskripce pohybové aktivity
+            <label htmlFor="">
+              <input
+                type="text"
+                name="titul"
+                id="titul"
+                value={inputs.titul || ''}
+                onChange={handleChange}
+                placeholder="Titul"
+                className="form-input"
+              />
             </label>
-            <input
-              type="checkbox"
-              name="individualni"
-              id="individualni"
-              onChange={handleChangeCheckbox}
-            />
-            <label for="individualni">Individuální cvičení</label>
-            <input
-              type="checkbox"
-              name="skupinove"
-              id="skupinove"
-              onChange={handleChangeCheckbox}
-            />
-            <label for="skupinove">Skupinové cvičení</label>
-            <input
-              type="checkbox"
-              name="nordic"
-              id="nordic"
-              onChange={handleChangeCheckbox}
-            />
-            <label for="nordic">Nordic walking</label>
-          </label>
+            <label htmlFor="">
+              <input
+                type="text"
+                name="jmeno"
+                id="jmeno"
+                value={inputs.jmeno || ''}
+                onChange={handleChange}
+                placeholder="Křestní jméno"
+                className="form-input"
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="text"
+                name="prijmeni"
+                id="prijmeni"
+                value={inputs.prijmeni || ''}
+                onChange={handleChange}
+                placeholder="Příjmení"
+                className="form-input"
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={inputs.email || ''}
+                onChange={handleChange}
+                placeholder="Email"
+                className="form-input"
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="tel"
+                name="mobil"
+                id="mobil"
+                pattern="[0-9]{9}"
+                value={inputs.mobil || ''}
+                onChange={handleChange}
+                placeholder="Mobil (+420123456789)"
+                className="form-input"
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="date"
+                name="datum"
+                id="datum"
+                value={inputs.datum || ''}
+                onChange={handleChange}
+                placeholder="Datum narození"
+                className="form-input"
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="number"
+                name="vyska"
+                id="vyska"
+                value={inputs.vyska || ''}
+                onChange={handleChange}
+                placeholder="Výška v cm"
+                className="form-input"
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="number"
+                name="hmotnost"
+                id="hmotnost"
+                value={inputs.hmotnost || ''}
+                onChange={handleChange}
+                placeholder="Hmotnost v kg"
+                className="form-input"
+              />
+            </label>
+          </div>
+
           <label htmlFor="">
-            Onemocnění:
             <textarea
               name="onemocneni"
               id="onemocneni"
@@ -157,10 +127,11 @@ export function EntryForm() {
               rows="8"
               value={inputs.onemocneni || ''}
               onChange={handleChange}
+              placeholder="Onemocnění"
+              className="form-input"
             ></textarea>
           </label>
           <label htmlFor="">
-            Léky (vypsat pouze názvy léků):
             <textarea
               name="leky"
               id="leky"
@@ -168,10 +139,11 @@ export function EntryForm() {
               rows="8"
               value={inputs.leky || ''}
               onChange={handleChange}
+              placeholder="Léky (vypsat pouze názvy léků)"
+              className="form-input"
             ></textarea>
           </label>
           <label htmlFor="">
-            Poznámky:
             <textarea
               name="poznamky"
               id="poznamky"
@@ -179,8 +151,52 @@ export function EntryForm() {
               rows="8"
               value={inputs.poznamky || ''}
               onChange={handleChange}
+              placeholder="Poznámky"
+              className="form-input"
             ></textarea>
           </label>
+          <label htmlFor="" className="mam-zajem">
+            Mám zájem o:
+            <div>
+              <input
+                type="checkbox"
+                name="konzultace"
+                id="konzultace"
+                onChange={handleChangeCheckbox}
+              />
+              <label for="konzultace">
+                Konzultace, testování, preskripce pohybové aktivity
+              </label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="individualni"
+                id="individualni"
+                onChange={handleChangeCheckbox}
+              />
+              <label for="individualni">Individuální cvičení</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="skupinove"
+                id="skupinove"
+                onChange={handleChangeCheckbox}
+              />
+              <label for="skupinove">Skupinové cvičení</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                name="nordic"
+                id="nordic"
+                onChange={handleChangeCheckbox}
+              />
+              <label for="nordic">Nordic walking</label>
+            </div>
+          </label>
+
           <input type="submit" value="Odeslat" />
         </form>
       </div>
